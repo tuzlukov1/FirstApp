9a3b9e87678f35e702411582092af94b8832236c
Yan Tuzlukov <tyzluckoff@yandex.ru>
2023-04-25 14:34:58 +0700

    git init

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    application
+}
+
+group = "me.tyzlu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("org.testng:testng:7.4.0"))
+    implementation(kotlin("io.rest-assured:rest-assured:4.4.0"))
+}
+
+dependencies {
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/fast_start/When.kt
@@ -0,0 +1,2 @@
+package fast_start
+
